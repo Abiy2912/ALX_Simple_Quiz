14f3debd28b1b40bb41c4c22d6757be76bef8453
Abiy Hailu <abiyhailu295@gmail.com>
2024-07-18 22:51:46 +0300

    thrid commit

--- quiz.js
+++ quiz.js
@@ -1,11 +1,12 @@
 document.getElementById('submit-answer').addEventListener('click', checkAnswer);
 
 function checkAnswer() {
+  const correctAnswer = '4';
     const selectedAnswer = document.querySelector('input[name="quiz"]:checked');
     
     const feedback = document.getElementById('feedback');
         
-      if (selectedAnswer.value === '4') {
+      if (selectedAnswer.value === correctAnswer) {
           feedback.textContent = 'Correct! Well done.';
           feedback.style.color = 'green';
       } else {
