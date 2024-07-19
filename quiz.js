f3e8b5411e463b1772e4f1c8d21e5ffe8dfefeca
Abiy Hailu <abiyhailu295@gmail.com>
2024-07-19 19:59:13 +0300

    8 commit

--- quiz.js
+++ quiz.js
@@ -1,12 +1,16 @@
-document.getElementById('submit-answer').addEventListener('click', checkAnswer);
+const submitButton = document.getElementById('submit-answer');
+submitButton.addEventListener('click', checkAnswer);
+
 
 function checkAnswer() {
-  const correctAnswer = '4';
-    const userAnswer = document.querySelector('input[name="quiz"]:checked');
+  const correctAnswer = "4";
+  
+    const selectedRadioButton = document.querySelector('input[name="quiz"]');
+    const userAnswer = selectedRadioButton.value;
     
     const feedback = document.getElementById('feedback');
         
-      if (userAnswer.value === Number(correctAnswer)) {
+      if (userAnswer === correctAnswer) {
           feedback.textContent = 'Correct! Well done.';
           feedback.style.color = 'green';
       } else {
